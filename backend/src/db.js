bc46be3f858004d9867ca0f23782d0a1d960cea7
syvolia <syvoliamary@gmail.com>
2020-04-14 02:19:24 +0300

    changes to prisma

--- backend/src/db.js
+++ backend/src/db.js
@@ -5,7 +5,7 @@ const { Prisma } = require('prisma-binding');
 
 const db = new Prisma({
   typeDefs: 'src/generated/prisma.graphql',
-  endpoint: 'https://sick-fits-production.herokuapp.com/sick-fits-prod/prod',
+  endpoint: process.env.PRISMA_ENDPOINT,
   secret: process.env.PRISMA_SECRET,
   debug: false
 });
